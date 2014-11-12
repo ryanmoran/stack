f8a41c326f8a909b8a37f895c3fee0329aa35200
Ryan Moran <rmoran@pivotal.io>
2014-11-12 11:53:22 -0800

    Replaces spaces with tabs

--- handler.go
+++ handler.go
@@ -3,19 +3,19 @@ package stack
 import "net/http"
 
 type Handler interface {
-    ServeHTTP(http.ResponseWriter, *http.Request, Context)
+	ServeHTTP(http.ResponseWriter, *http.Request, Context)
 }
 
 type compatibleHandler struct {
-    handler http.Handler
+	handler http.Handler
 }
 
 func CompatibleHandler(handler http.Handler) compatibleHandler {
-    return compatibleHandler{
-        handler: handler,
-    }
+	return compatibleHandler{
+		handler: handler,
+	}
 }
 
 func (handler compatibleHandler) ServeHTTP(w http.ResponseWriter, req *http.Request, context Context) {
-    handler.handler.ServeHTTP(w, req)
+	handler.handler.ServeHTTP(w, req)
 }
--- init_test.go
+++ init_test.go
@@ -1,13 +1,13 @@
 package stack_test
 
 import (
-    "testing"
+	"testing"
 
-    . "github.com/onsi/ginkgo"
-    . "github.com/onsi/gomega"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
 func TestWebSuite(t *testing.T) {
-    RegisterFailHandler(Fail)
-    RunSpecs(t, "Stack Suite")
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Stack Suite")
 }
